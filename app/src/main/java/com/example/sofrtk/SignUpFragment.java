585dedbcb77f03927230c2a81f32793c7cd1bb43
Adham <adhambiko22@gmail.com>
2025-02-07 02:39:31 +0200

    Login & Signup Validation

--- app/src/main/java/com/example/sofrtk/SignUpFragment.java
+++ app/src/main/java/com/example/sofrtk/SignUpFragment.java
@@ -2,57 +2,38 @@ package com.example.sofrtk;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SignUpFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.lang.reflect.Array;
+
 public class SignUpFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TextView loginTxt;
+    EditText firstNameTextField;
+    EditText lastNameTextField;
+    EditText emailTextField;
+    EditText passwordTextField;
+    EditText confirmPasswordTextField;
+    Button btnRegister;
 
     public SignUpFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SignUpFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SignUpFragment newInstance(String param1, String param2) {
-        SignUpFragment fragment = new SignUpFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -61,4 +42,62 @@ public class SignUpFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_up, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        loginTxt = view.findViewById(R.id.loginTxt);
+        loginTxt.setOnClickListener(v -> {
+            Navigation.findNavController(view).navigate(R.id.action_signUpFragment_to_loginFragment);
+        });
+
+        firstNameTextField = view.findViewById(R.id.firstNameTextField);
+        lastNameTextField = view.findViewById(R.id.lastNameTextField);
+        emailTextField = view.findViewById(R.id.emailTextField);
+        passwordTextField = view.findViewById(R.id.passwordTextField);
+        confirmPasswordTextField = view.findViewById(R.id.confirmPasswordTextField);
+        btnRegister = view.findViewById(R.id.btnRegister);
+
+        EditText[] editTextFields = {firstNameTextField,lastNameTextField,emailTextField,passwordTextField,confirmPasswordTextField};
+        btnRegister.setOnClickListener(v -> {
+            for(int i=0;i<5;i++){
+                validateEmptyEditText(editTextFields[i]);
+            }
+
+            if(firstNameTextField.getText().length()<3){
+                firstNameTextField.setError("The Name Cannot Be Less Than 3 Characters");
+            }
+
+            if(lastNameTextField.getText().length()<3){
+                lastNameTextField.setError("The Name Cannot Be Less Than 3 Characters");
+            }
+
+            if(passwordTextField.getText().length()<6){
+                passwordTextField.setError("The Password Cannot Be Less Than 6 Characters");
+            }
+
+            if(confirmPasswordTextField.getText().length()<6){
+                confirmPasswordTextField.setError("The Password Cannot Be Less Than 6 Characters");
+            }
+
+            if(passwordTextField.getText().length()>=6 && confirmPasswordTextField.getText().length()>=6){
+                if(!passwordTextField.getText().toString().equals(confirmPasswordTextField.getText().toString())){
+                    confirmPasswordTextField.setError("Password Must Be The Same");
+                }
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(emailTextField.getText().toString().trim()).matches()) {
+                emailTextField.setError("Invalid Email Format");
+            }
+        });
+
+
+    }
+    public void validateEmptyEditText(EditText field){
+        if(field.getText().toString().trim().isEmpty()){
+            field.setError("This Field Cannot Be Empty");
+        }
+    }
+
 }
--- app/src/main/java/com/example/sofrtk/SplashFragment.java
+++ app/src/main/java/com/example/sofrtk/SplashFragment.java
@@ -1,14 +1,19 @@
 package com.example.sofrtk;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.ImageView;
 
 import com.airbnb.lottie.LottieAnimationView;
@@ -40,8 +45,22 @@ public class SplashFragment extends Fragment {
         logo = view.findViewById(R.id.sofrtkLogo);
         lottieAnimationView = view.findViewById(R.id.lottie);
 
-        logo.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
-        //lottieAnimationView.animate().translationY(1400).setDuration(6000).setStartDelay(4000);
+        //logo.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        logo.setAlpha(0f);
+        logo.setScaleX(0.5f);
+        logo.setScaleY(0.5f);
+        logo.setTranslationY(100f);
+
+        logo.animate()
+                .scaleX(1f)
+                .scaleY(1f)
+                .alpha(1f)
+                .translationY(0)
+                .setDuration(1200)
+                .setStartDelay(500)
+                .setInterpolator(new AccelerateDecelerateInterpolator());
+
+
         lottieAnimationView.animate()
                 .translationY(0)
                 .setDuration(1000)
@@ -50,5 +69,13 @@ public class SplashFragment extends Fragment {
                 .withEndAction(() -> {
                     lottieAnimationView.playAnimation();
                 });
+
+        lottieAnimationView.addAnimatorListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                NavController navController = Navigation.findNavController(view);
+                navController.navigate(R.id.action_splashFragment_to_signUpFragment);
+            }
+        });
     }
 }
