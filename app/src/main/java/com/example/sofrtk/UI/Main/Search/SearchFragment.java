1a10220051887404a6055d68fc5ca66043ab1fcd
Adham <adhambiko22@gmail.com>
2025-02-10 21:56:55 +0200

    Initial Search Fragment UI

---
+++ app/src/main/java/com/example/sofrtk/UI/Main/Search/SearchFragment.java
@@ -0,0 +1,46 @@
+package com.example.sofrtk.UI.Main.Search;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sofrtk.Adapters.CategoryAdapter;
+import com.example.sofrtk.Pojos.Category;
+import com.example.sofrtk.R;
+
+import java.util.ArrayList;
+
+public class SearchFragment extends Fragment {
+
+
+    public SearchFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
